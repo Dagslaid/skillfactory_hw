4989d04b05e73b0888e9aa6749d525d221c0bfe1
Dagslaid <123592981+Dagslaid@users.noreply.github.com>
2023-01-29 17:24:03 +0300

    Solution tasks

--- game_v2.py
+++ game_v2.py
@@ -15,15 +15,21 @@ def random_predict(number: int = 1) -> int:
         int: Число попыток
     """
     count = 0
-
+    num_1 , num_2 = 1, 100 #Интервал чисел
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
-            break  # выход из цикла если угадали
+        predict_number = np.random.randint(num_1 , num_2) # Генерация числа
+        
+        if number > predict_number:
+            num_1 = predict_number     
+        elif number < predict_number:
+            num_2 = predict_number 
+        else: 
+            number == predict_number
+            break    
+    
     return count
-
-
+ 
 def score_game(random_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
@@ -34,8 +40,8 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 100, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
@@ -45,6 +51,6 @@ def score_game(random_predict) -> int:
     return score
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+score_game(random_predict)
